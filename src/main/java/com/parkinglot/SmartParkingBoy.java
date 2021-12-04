462cfd1939cc8d2292f04dbf893fe7dc6bbcf97d
moksi3 <simon.mok@oocl.com>
2021-12-04 11:37:56 +0800

    [parking lot][simon] test: add should_return_lot1_ticket_when_parkCar_given_two_lot_with_same_remaining_slot

---
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,17 @@
+package com.parkinglot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class SmartParkingBoy {
+    ArrayList<ParkingLot> lots;
+    public SmartParkingBoy(ArrayList<ParkingLot> lots)
+    {
+        this.lots = lots;
+    }
+
+    public Ticket parkCar(Car car)
+    {
+        return null;
+    }
+}
